de7ba7ffa3c5aa845be1d8843a0a066d2a383c68
vitorsantabi <98997905+vitorsantabi@users.noreply.github.com>
2024-10-16 18:43:03 -0300

    Add files via upload

--- Hackathon/quiz.js
+++ Hackathon/quiz.js
@@ -6,6 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const faixaResultado = document.getElementById("faixa-resultado");
     const nomeInput = document.getElementById("nome");
 
+    // Respostas corretas
     const respostasCorretas = {
         Q1: "Op1",
         Q2: "O3",
@@ -18,16 +19,17 @@ document.addEventListener("DOMContentLoaded", () => {
         Q9: "E3",
         Q10: "F1",
         Q11: "Um tipo de malware que registra teclas digitadas em um teclado para roubar informações",
-        Q12: "Detectar e remover malware do sistema",
-        Q13: "Manipulação psicológica de pessoas para obter informações úteis",
-        Q14: "Gerenciamento de informações e eventos de segurança",
-        Q15:  "Firewalls, Antivírus e Antimalware, Criptografia,  Autenticação Multifator (MFA), Backups de Dados, Treinamento de Conscientização em Segurança, Monitoramento de Rede, Segurança em Nuvem, Políticas de Segurança, Respostas a Incidentes, Gerenciamento de Patches",
+Q12: "Detectar e remover malware do sistema",
+Q13: "Manipulação psicológica de pessoas para obter informações úteis",
+Q14: "Gerenciamento de informações e eventos de segurança",
+Q15:  "Firewalls, Antivírus e Antimalware, Criptografia,  Autenticação Multifator (MFA), Backups de Dados, Treinamento de Conscientização em Segurança, Monitoramento de Rede, Segurança em Nuvem, Políticas de Segurança, Respostas a Incidentes, Gerenciamento de Patches",
     };
 
     form.addEventListener("submit", function (event) {
-        event.preventDefault();
+        event.preventDefault(); // Evita o envio do formulário
         let pontuacao = 0;
 
+        // Calcula a pontuação
         for (const [questao, respostaCorreta] of Object.entries(respostasCorretas)) {
             const respostaSelecionada = form.querySelector(`input[name="${questao}"]:checked`);
             if (respostaSelecionada && respostaSelecionada.id === respostaCorreta) {
@@ -35,18 +37,20 @@ document.addEventListener("DOMContentLoaded", () => {
             }
         }
 
+        // Exibe o resultado
         const nome = nomeInput.value || "Anônimo";
         nomeResultado.textContent = `Seu nome: ${nome}`;
         pontuacaoResultado.textContent = `Você acertou ${pontuacao} de ${Object.keys(respostasCorretas).length} perguntas.`;
 
+        // Define a faixa
         if (pontuacao <= 5) {
-            faixaResultado.textContent = "Faixa: Branca";
+            faixaResultado.textContent = "Faixa: Branco";
         } else if (pontuacao <= 8) {
             faixaResultado.textContent = "Faixa: Marrom";
         } else {
             faixaResultado.textContent = "Faixa: Preta";
         }
 
-        resultadoDiv.style.display = "block";
+        resultadoDiv.style.display = "block"; // Exibe a div de resultado
     });
 });
